e8bba3cc123b0f2ea2328f10f13e3b42e2180beb
Efrat Finkel <efrat4059@gmai.com>
2022-07-31 14:24:38 +0300

    initial compression works

--- CompressionAndDecompression/CompressionAndDecompression/compress.c
+++ CompressionAndDecompression/CompressionAndDecompression/compress.c
@@ -1,13 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "dictionary.h"
-#include "compare.h"
+#include "compress.h"
+#include "binaryFile.h"
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "binaryFile.h"
-#include "detailsStruct.h"
-#define maxBits 12
 
+Sequence* replaceAppendSequence(Sequence* s, unsigned char c) {
+	Sequence* temp = copySequenceAppend(s, c);
+	deleteSequence(s);
+
+	return temp;
+}
 //initialize a dictionary:
 //first 256 places for ascii code
 Dict* initializeDict(unsigned int size) {
@@ -22,39 +26,47 @@ Dict* initializeDict(unsigned int size) {
 }
 
 //for the logging file
-extern struct Details details;
+
 void compression(FILE* fpSource, FILE* fpOutput) {
 	//store the next character/byte
-	unsigned char character;
+	unsigned int character;
 	//will be the output eventually
 	unsigned int code;
 	//initialize the table for 12 bits
-	unsigned int tableSize = 1 << maxBits;
+	unsigned int tableSize = 1 << 12;
+	printf("table size %d \n", tableSize);
 	Dict* dict = initializeDict(tableSize);
 	//keep track of table entries
 	int nextCode = 256;
 	character = getc(fpSource);
 	Sequence* str = newSequence(character);
+
 	//or c!=-1?
 	while (character != EOF) {
 		character = getc(fpSource);
 		Sequence* expandedStr = copySequenceAppend(str, character);
+		puts(expandedStr->data);
+
 		//if sequence is in the dict
 		if (searchDict(dict, expandedStr, &code)) {
+			printf("found in first search: %s\n", expandedStr->data);
 			deleteSequence(str);
 			str = expandedStr;
+			puts(str->data);
 		}
 		//sequence is not in dict
 		else {
 			//searching without the next character
 			searchDict(dict, str, &code);
+			printf("found str: %s\n", str->data);
 			//output 
 			// the code
+			printf("code %d \n", code);
 			writeBinary(fpOutput, code);
 			/*if (nextCode >= (1 << (bits)) && bits < maxBits) {
 				bits++;
 			}*/
-			//check table size if it's okay, insert to dict
+			//check table size. if it's okay, insert to dict
 			if (nextCode < tableSize) {
 				insertDict(dict, expandedStr, nextCode++);
 
@@ -68,12 +80,13 @@ void compression(FILE* fpSource, FILE* fpOutput) {
 		}
 	}
 	searchDict(dict, str, &code);
-	writeBinary(fpOutput, code);
+	/*writeBinary(fpOutput, code);*/
 	deleteSequence(str);
 	deleteDictDeep(dict);
 	//close output file
 	//decompression
 	//comparison
+	fclose(fpOutput);
 
 }
 
