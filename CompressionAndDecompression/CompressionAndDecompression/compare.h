9abd1c79ff8a6d54e47a01c7d983b2b251bf534e
ayala <a0534183195@gmail.com>
2022-08-02 23:23:48 +0300

    comparison, some functions on files,and calculations.

--- CompressionAndDecompression/CompressionAndDecompression/compare.h
+++ CompressionAndDecompression/CompressionAndDecompression/compare.h
@@ -1,3 +1,17 @@
 #pragma once
 #include <stdio.h>
-int compare(FILE* fpSource, FILE* fpOutput);
+char* calcTime();
+int fileSize(FILE* fp);
+int wrapDecompression(FILE* fpCompressed);
+int areFileSizesEquals(int sourceFileSize, int decompressedFileSize);
+int chComparison(FILE* fpSource);
+int kComparison(FILE* fpSource, FILE* fpDecompressed);
+int mComparison(FILE* fpSource, FILE* fpDecompressed);
+int goToTheRelevantComparison(int sizeFile, FILE* fpSouce, FILE* fpDeconpressed);
+int wrapCompare(FILE* fpSource, FILE* fpCompressed);
+
+
+
+
+
+
