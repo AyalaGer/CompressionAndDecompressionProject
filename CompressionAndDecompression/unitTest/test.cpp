d2f5ab8fd6d108b69ab5bc48c0dffb9e4db25a61
Efrat Finkel <efrat4059@gmai.com>
2022-08-09 11:50:29 +0300

    test

--- CompressionAndDecompression/unitTest/test.cpp
+++ CompressionAndDecompression/unitTest/test.cpp
@@ -1,6 +1,29 @@
 #include "pch.h"
-
+#include "../CompressionAndDecompression/filesHandling.h"
+#include "../CompressionAndDecompression/filesHandling.c"
+#include "../CompressionAndDecompression/detailsStruct.h"
+#include "../CompressionAndDecompression/detailsStruct.c"
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+} 
+//Open files
+TEST(FileHandling, OpenFile) {
+	FILE* fp;
+	char* path="txt.txt";
+	char* path1 = "txt1.txt";
+	//Open a valid file.
+	EXPECT_EQ(1,openFile(path,&fp,"a+"));
+	//Open an unexisting file in read mode.
+	EXPECT_EQ(0, openFile(path1, &fp, "r"));
+	//Open an invalid file.
+	EXPECT_EQ(0, openFile("C:\\Users\\Efrat\\Downloading\\short.txt", &fp, "a+"));
+}
+TEST(FileHandling, CloseFile) {
+	FILE* fp;
+	char* path = "txt.txt";
+	char* path1 = "txt1.txt";
+	openFile(path, &fp, "a+");
+	//Close a valid file.
+	EXPECT_EQ(1, closeFile(fp));
 }
